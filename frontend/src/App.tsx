e3b2f65ae6f4cfe9a42c5609d11143bf17ce0de3
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2024-02-24 14:29:32 +0700

    ui ticket

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Employee from "./pages/employee";
 import Home from "./pages/home";
+import Ticket from "./pages/ticket";
+import Payment from "./pages/payment";
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/ticket" element={<Ticket/>}/>
+        <Route path="/payment" element={<Payment/>}/>
+        <Route path="/employee" element={<Employee/>}/>
       </Routes>
     </>
   );
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -8,6 +8,9 @@ import {
     CarouselPrevious,
 } from "@cn/components/ui/carousel"
 import { Button } from "@cn/components/ui/button"
+//import { Navigate } from "react-router-dom"
+//import Tour1 from "@src/assets/tour1.jpg";
+
 // import {
 //     Select,
 //     SelectContent,
@@ -71,13 +74,19 @@ const Home = () => {
                                 </CardContent>
                             </Card>
                         </div>
-                        <Button variant="outline" className="ml-10">Search</Button>
+                        <Button variant="outline" className="ml-10" >Search</Button>
                     </TabsContent>
                 </div>
-                <TabsContent value="Contact">
-                    <div className="m-5">
-                        <span className="text-xl font-semibold m-5">ติดต่อเรา</span>
-                        <span></span>
+                <TabsContent value="Contact" className="grid grid-cols-2 gap-4">
+                    {/* <div><img src={Tour1} alt="Tour1" className="m-5 rounded-lg w-96 h-72"></img></div> */}
+                    <div className="m-5 ">
+                        <Card>
+                            <div className="text-xl font-semibold m-5">Contact</div>
+                            <div className="text-l font-semibold m-7">Address:</div>
+                            <div className="text-l font-semibold m-7">Email: B6400415@g.sut.ac.th</div>
+                            <div className="text-l font-semibold m-7">Tel-1: 0933966012</div>
+                            <div className="text-l font-semibold m-7">Tel-2: 0971037596</div>
+                        </Card>
                     </div>
                 </TabsContent>
             </Tabs>
